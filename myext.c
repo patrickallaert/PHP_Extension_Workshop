47695896ee18fe1918dee2cc65553a5274e5f44c
Patrick Allaert <pa@ez.no>
2011-09-25 13:39:44 +0200

    Added: minimal extension code

---
+++ myext.c
@@ -0,0 +1,29 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+#include "php_ini.h"
+#include "ext/standard/info.h"
+#include "php_myext.h"
+
+
+/* {{{ myext_module_entry
+ */
+zend_module_entry myext_module_entry = {
+    STANDARD_MODULE_HEADER,
+    "myext",
+    NULL, /* Function entries */
+    NULL, /* Module init */
+    NULL, /* Module shutdown */
+    NULL, /* Request init */
+    NULL, /* Request shutdown */
+    NULL, /* Module information */
+    "0.1", /* Replace with version number for your extension */
+    STANDARD_MODULE_PROPERTIES
+};
+/* }}} */
+
+#ifdef COMPILE_DL_MYEXT
+ZEND_GET_MODULE(myext)
+#endif
