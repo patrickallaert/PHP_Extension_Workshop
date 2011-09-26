b17e7fdec9bca558e45a96cf1aba08eaaf5ad0da
Patrick Allaert <pa@ez.no>
2011-09-26 09:14:45 +0200

    Added: constants read from INI

--- myext.c
+++ myext.c
@@ -3,6 +3,7 @@
 #endif
 
 #include "php.h"
+#include "php_ini.h"
 #include "php_myext.h"
 
 
@@ -13,7 +14,7 @@ zend_module_entry myext_module_entry = {
     "myext",
     NULL, /* Function entries */
     PHP_MINIT(myext), /* Module init */
-    NULL, /* Module shutdown */
+    PHP_MSHUTDOWN(myext), /* Module shutdown */
     NULL, /* Request init */
     NULL, /* Request shutdown */
     NULL, /* Module information */
@@ -26,8 +27,24 @@ zend_module_entry myext_module_entry = {
 ZEND_GET_MODULE(myext)
 #endif
 
+PHP_INI_BEGIN()
+    PHP_INI_ENTRY("myext.pi", NULL, PHP_INI_SYSTEM, NULL)
+    PHP_INI_ENTRY("myext.euler", NULL, PHP_INI_SYSTEM, NULL)
+    PHP_INI_ENTRY("myext.golden", NULL, PHP_INI_SYSTEM, NULL)
+PHP_INI_END()
+
 PHP_MINIT_FUNCTION(myext)
 {
-    REGISTER_DOUBLE_CONSTANT("MYEXT_PI", 3.14159265358979, CONST_CS | CONST_PERSISTENT);
+    REGISTER_INI_ENTRIES();
+
+    REGISTER_DOUBLE_CONSTANT("MYEXT_PI", INI_FLT("myext.pi"), CONST_CS | CONST_PERSISTENT);
+    REGISTER_DOUBLE_CONSTANT("MYEXT_EULER", INI_FLT("myext.euler"), CONST_CS | CONST_PERSISTENT);
+    REGISTER_DOUBLE_CONSTANT("MYEXT_GOLDEN", INI_FLT("myext.golden"), CONST_CS | CONST_PERSISTENT);
+    return SUCCESS;
+}
+
+PHP_MSHUTDOWN_FUNCTION(myext)
+{
+    UNREGISTER_INI_ENTRIES();
     return SUCCESS;
 }
--- myext.php
+++ myext.php
@@ -1,3 +1,5 @@
 <?php
 
 echo MYEXT_PI, "\n";
+echo MYEXT_EULER, "\n";
+echo MYEXT_GOLDEN, "\n";
